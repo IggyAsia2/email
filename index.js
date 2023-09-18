7f46ed1bc15520e780329a6257f8a3c127a087f1
IggyAsia2 <luongxuanbao@gmail.com>
2023-09-18 16:05:26 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const port = 4001
+
+app.get('/', (req, res)=> {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+app.listen(port);
